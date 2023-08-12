df54d8cb5b32daea059ac03a5be8b27195539b62
Panto! <guustavopantoja@gmail.com>
2023-08-12 17:24:33 -0300

    chore: change const for the new engine repo

--- utils/const.js
+++ utils/const.js
@@ -2,5 +2,5 @@ export const GITHUB_API_ROOT = "https://api.github.com/";
 
 export const YARG_ORGANIZATIONNAME = "YARC-Official";
 export const YARG_GAMEREPOSITORY = "YARG";
-export const YARG_BLEEDINGEDGEREPOSITORY = "YARG-BleedingEdge";
+export const YARG_BLEEDINGEDGEREPOSITORY = "YARG-NewEngine";
 export const YARG_DEVBRANCH = "rewrite";
