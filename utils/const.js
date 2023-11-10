51cf8f6c061e465e8088e9074a44769b0977b849
Panto! <guustavopantoja@gmail.com>
2023-11-10 00:52:42 -0300

    chore: clean repo

--- utils/const.js
+++
@@ -1,6 +0,0 @@
-export const GITHUB_API_ROOT = "https://api.github.com/";
-
-export const YARG_ORGANIZATIONNAME = "YARC-Official";
-export const YARG_GAMEREPOSITORY = "YARG";
-export const YARG_BLEEDINGEDGEREPOSITORY = "YARG-BleedingEdge";
-export const YARG_DEVBRANCH = "dev";
